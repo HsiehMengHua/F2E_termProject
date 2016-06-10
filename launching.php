5ccaccc3fea131f2aae3ffce25c3432a1729c517
Hsieh Meng Hua <qprs71@gmail.com>
2016-06-10 22:23:21 +0800

    報導頁L

---
+++ launching.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+include("connectDB.php");
+
+$location = $_POST["location"];
+$date = $_POST["date"];
+$time = $_POST["time"];
+$desc = $_POST["description"];
+$id = $_SESSION["member_id"];
+
+$sql_insert = "INSERT INTO `activity` (`id`, `initiator_id`, `date`, `time`, `location`, `description`) VALUES (NULL, '', '$date', '$time', '$location', '$desc')";
+
+if($conn->query($sql_insert)){
+	
+	echo "data inserted";/*
+	// Redirect
+	$host  = $_SERVER['HTTP_HOST'];
+	$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$extra = "activities.php";
+	header("Location: http://$host$uri/$extra");
+	exit();*/
+}else{
+	echo "Error: ".$conn->error;
+}
+
+?>
