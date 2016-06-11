e719863890631e7ce887e864e728687ba8e8ed71
Hsieh Meng Hua <qprs71@gmail.com>
2016-06-11 23:43:28 +0800

    從DB抓資料顯示、記錄瀏覽次數、sidebar顯示熱門報導

--- pages/issue/issue_article.php
+++ pages/issue/issue_article.php
@@ -1,8 +1,42 @@
+<?php
+
+include("../connectDB.php");
+
+$sql = "SELECT * FROM `issue` WHERE `id` = ".$_GET['id'];
+$row = $conn->query($sql)->fetch_assoc();
+
+$issue_id = $row['id'];
+$title = $row['title'];
+$datetime = $row['release_datetime'];
+$source = $row['source'];
+$image_path = $row['image_path'];
+$html_path = $row['html_path'];
+$views = $row['views'];
+
+$sql_currentViews = "SELECT `views` FROM `issue` WHERE `id` = $issue_id";
+$row_views = $conn->query($sql_currentViews)->fetch_assoc();
+$count = $row_views["views"]+1;
+$sql_countViews = "UPDATE `issue` SET `views` = '$count' WHERE `id` = $issue_id";
+$conn->query($sql_countViews);
+
+// 選一個月內瀏覽次數最高的前7個報導，sidebar用
+$sql_popular = "SELECT `id`,`title` FROM `issue` WHERE `release_datetime` BETWEEN DATE_SUB(release_datetime,INTERVAL 30 DAY) AND NOW() ORDER BY `views` DESC LIMIT 7";
+$result_popular = $conn->query($sql_popular);
+$popularIdList = [];
+$popularTitleList = [];
+while($row_popular = $result_popular->fetch_assoc()){
+  array_push($popularIdList,$row_popular['id']);
+  array_push($popularTitleList,$row_popular['title']);
+} // 把多項結果存成陣列
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="zh">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title><?php echo $title; ?></title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="../../css/style.css" />
   <link rel="stylesheet" href="../../css/issue_article.css" />
@@ -21,15 +55,15 @@
       <div class="clear">
         <div class="main-content">
           <div class="heading">
-            <h1>你買的是沐浴乳還是塑膠微粒？快上網查App</h1>
+            <h1><?php echo $title; ?></h1>
             <div class="article-info clear">
-              <p><i class="material-icons">schedule</i>  2016-06-10  17:36</p>
-              <p class="pull-right">上下游新聞市集</p>
+              <p><i class="material-icons">schedule</i>  <?php echo $datetime; ?>，<?php echo $source; ?></p>
+              <p class="pull-right"><i class="material-icons">visibility</i>  <?php echo $views; ?></p>
             </div>
           </div>
-          <img src="../../img/img1.jpg" alt="">
+          <img src="<?php echo $image_path; ?>" alt="<?php echo $title; ?>">
           <article>
-            <div w3-include-html="example_article.html"></div>  
+            <div w3-include-html="<?php echo $html_path; ?>"></div>
           </article>
           <div class="other_news">
             <div class="container clear">
@@ -50,13 +84,11 @@
         <div class="sidebar pull-right">
           <h4>大家在關注的報導</h4>
           <ul>
-            <li><a href="">微塑膠成為幼魚「新食物」</a></li>
-            <li><a href="">幼魚肚裡滿滿柔珠研究證實影響成長</a></li>
-            <li><a href="">微塑膠成為幼魚「新食物」</a></li>
-            <li><a href="">幼魚肚裡滿滿柔珠研究證實影響成長</a></li>
-            <li><a href=""><a href="">微塑膠成為幼魚「新食物」</a></li>
-            <li><a href="">幼魚肚裡滿滿柔珠研究證實影響成長</a></li>
-            <li><a href="">微塑膠成為幼魚「新食物」</a></li>
+            <?php
+            for($i=0; $i<sizeof($popularIdList); $i++){
+              echo '<li><a href="issue_article.php?id='.$popularIdList[$i].'">'.$popularTitleList[$i].'</a></li>';
+            }
+            ?>
           </ul>
         </div>
       </div>
